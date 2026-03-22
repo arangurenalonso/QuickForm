f775aa9872616a065138776be1712cf88893becc
arangurenalonso <69709834+arangurenalonso@users.noreply.github.com>
2026-03-22 09:22:42 -0700

    Feature/OrderModalType

--- src/modules/ui/store/modal/modal.type.ts
+++ src/modules/ui/store/modal/modal.type.ts
@@ -1,20 +1,20 @@
 export enum ModalId {
+  RESEND_VERIFICATION_SUCCESS = 'resend-verification-success-modal',
+  FORGOT_PASSWORD_SUCCESS = 'forgot-password-success-modal',
+  RESET_PASSWORD_SUCCESS = 'reset-password-success-modal',
+  REGISTER_SUCCESS = 'register-success-modal',
   CREATE_FORM = 'create-form-modal',
   SUBMIT_FORM = 'submit-form-modal',
   SUBMIT_PREVIEW = 'submit-preview-modal',
-  RESET_PASSWORD_SUCCESS = 'reset-password-success-modal',
-  RESEND_VERIFICATION_SUCCESS = 'resend-verification-success-modal',
-  REGISTER_SUCCESS = 'register-success-modal',
-  FORGOT_PASSWORD_SUCCESS = 'forgot-password-success-modal',
 }
 
 export enum ModalErrorType {
+  RESENT_VERIFY_EMAIL_ERROR = 'resent-verify-email-error-modal',
+  FORGOT_PASSWORD_ERROR = 'forgot-password-error-modal',
+  RESET_PASSWORD_ERROR = 'reset-password-error-modal',
+  REGISTER_ERROR = 'register-error-modal',
   CREATE_FORM_ERROR = 'create-form-error-modal',
   EMAIL_CONFIRMATION_ERROR = 'email-confirmation-error-modal',
-  FORGOT_PASSWORD_ERROR = 'forgot-password-error-modal',
   LOGIN_ERROR = 'login-error-modal',
-  REGISTER_ERROR = 'register-error-modal',
-  RESENT_VERIFY_EMAIL_ERROR = 'resent-verify-email-error-modal',
-  RESET_PASSWORD_ERROR = 'reset-password-error-modal',
   PUBLISH_FORM_ERROR = 'publish-form-error-modal',
 }
